3580fd48791ff29350914f0db1c0f3e613b13254
JJG <jjeromegerstch@gmail.com>
2017-03-30 22:38:25 +0200

    20170330_BallBounce  Found a way of preventing the ball to bounce : http://answers.unity3d.com/questions/217639/physics-objects-passing-through-each-otherbouncing.html

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	private Rigidbody m_Rigidbody;
+	public float m_Speed = 100;
+	private AudioSource m_AudioSource;
+
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 1000);
+		m_Rigidbody = GetComponent<Rigidbody>();
+		m_AudioSource = GetComponent<AudioSource>();
+		m_Rigidbody.velocity = new Vector3(0, 0, m_Speed);
+		m_AudioSource.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void FixedUpdate()
+	{
+		var currentVelocity = m_Rigidbody.velocity;
+
+		if (currentVelocity.y <= 0f)
+			return;
+
+		currentVelocity.y = 0f;
+
+		m_Rigidbody.velocity = currentVelocity;
 	}
 }
